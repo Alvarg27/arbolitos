7463cf9b19ab86322bbeccd449cf801a649833a7
Alvarg27 <alvargbesne@gmail.com>
2022-09-10 14:07:59 -0500

    update

--- components/Hero.js
+++ components/Hero.js
@@ -1,4 +1,6 @@
+import { Transition } from "@headlessui/react";
 import React, { useState, useRef, useEffect } from "react";
+import { FaPlay } from "react-icons/fa";
 
 const Hero = () => {
   const [playing, setPlaying] = useState(false);
@@ -28,17 +30,33 @@ const Hero = () => {
   return (
     <div className="w-full">
       <div className="mt-[50px]">
-        <video
-          onClick={() => handleVideoPress()}
-          poster="/poster.webp"
-          className="max-w-[800px] m-auto cursor-pointer"
-          playsInline
-          loop
-          muted
-          ref={videoRef}
-        >
-          <source src="/arbolitos.mp4" />
-        </video>
+        <div className="w-full max-w-[800px] h-[300px] lg:h-[400px] m-auto bg-gray-200 relative">
+          <Transition
+            show={!playing}
+            enter="transition-opacity duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="transition-opacity duration-300"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="absolute w-full h-full flex bg-black bg-opacity-30">
+              <FaPlay className="m-auto text-6xl text-white opacity-50" />
+            </div>
+          </Transition>
+
+          <video
+            onClick={() => handleVideoPress()}
+            poster="/poster.webp"
+            className="m-auto cursor-pointer w-full h-full object-cover"
+            playsInline
+            loop
+            muted
+            ref={videoRef}
+          >
+            <source src="/arbolitos.mp4" />
+          </video>
+        </div>
       </div>
     </div>
   );
