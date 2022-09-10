903ad880b41aa4b3bce20edea2d5e0b35713b1fa
Alvarg27 <alvargbesne@gmail.com>
2022-09-10 14:57:07 -0500

    Update FeaturedProducts.js

--- components/FeaturedProducts.js
+++ components/FeaturedProducts.js
@@ -6,8 +6,8 @@ const FeaturedProducts = () => {
     <div className="my-10">
       <div className="flex flex-col">
         <p className="font-serif text-2xl m-auto">Platillos destacados</p>
-        <div className="mt-4 flex overflow-auto no-scrollbar m-auto">
-          <div className="min-w-[300px] text-center py-4 px-2 shadow-lg">
+        <div className="py-4 flex overflow-auto no-scrollbar m-auto">
+          <div className="min-w-[300px] text-center py-4 px-2 shadow-lg m-2">
             <Image
               src="/lengua.webp"
               layout="responsive"
@@ -18,7 +18,7 @@ const FeaturedProducts = () => {
             <p className="text-md mt-4">Lengua de res en salsa de la casa</p>
             <p className="text-sm text-gray-500">Nuestra especialidad</p>
           </div>
-          <div className="min-w-[300px] text-center py-4 px-2 shadow-lg">
+          <div className="min-w-[300px] text-center py-4 px-2 shadow-lg m-2">
             <Image
               src="/sopa-azteca.webp"
               layout="responsive"
