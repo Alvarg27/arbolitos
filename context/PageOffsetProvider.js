85937b05dd52999c6da9f9b371875efb8731c4db
Alvarg27 <alvargbesne@gmail.com>
2022-09-12 22:51:00 -0500

    Update PageOffsetProvider.js

--- context/PageOffsetProvider.js
+++ context/PageOffsetProvider.js
@@ -7,6 +7,29 @@ export const PageOffsetProvider = ({ children }) => {
   const handleScroll = () => setOffsetY(window.pageYOffset);
 
   useEffect(() => {
+    document.addEventListener("DOMContentLoaded", function (event) {
+      function debounce(func, wait, immediate) {
+        var timeout;
+        return function () {
+          var context = this,
+            args = arguments;
+          var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+          };
+          var callNow = immediate && !timeout;
+          clearTimeout(timeout);
+          timeout = setTimeout(later, wait);
+          if (callNow) func.apply(context, args);
+        };
+      }
+
+      var myEfficientFn = debounce(function () {
+        console.log("HEY STOP MOVING ME AROUND!");
+      }, 25);
+
+      window.addEventListener("mousemove", myEfficientFn), false;
+    });
     window.addEventListener("scroll", handleScroll);
 
     return () => window.removeEventListener("scroll", handleScroll);
