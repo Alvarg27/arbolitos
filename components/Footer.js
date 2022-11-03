6a616edd42496853e66cfc672f564d5ff9f55c34
Alvarg27 <alvargbesne@gmail.com>
2022-11-02 18:45:58 -0600

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
     <div className="relative bg-black">
       <div className="flex flex-col  mt-8">
         <div className="text-center mx-auto flex">
-          <div className="m-auto flex flex-col">
+          <div className="m-auto flex flex-col w-full relative">
             <div className="m-auto">
               <Image
                 src={logo}
@@ -23,18 +23,46 @@ const Footer = () => {
                 priority={true}
               />
             </div>
-            <div className="flex mx-auto">
-              <FaPhone className="text-white my-auto mr-2" />
-              <p className="text-gray-500 my-auto">773 724 5263</p>
+            <div>
+              <div className="flex flex-col bg-neutral-900 rounded-xl p-4 flex-1 m-4">
+                <p className="text-white font-serif text-lg">Tula, Hgo.</p>
+                <p className="text-xs mb-4 text-gray-500 text-center max-w-[80%]  mx-auto ">
+                  Carretera Tula-Refineria km 31.79, El Llano 2da Secc, 42803
+                  Tula de Allende, Hgo.
+                </p>
+                <Link href="https://g.page/losarbolitosrestaurante?share">
+                  <a target="_blank">
+                    <Button className="bg-white mx-auto mb-2 px-8 text-sm ">
+                      ¿Cómo llegar?
+                    </Button>
+                  </a>
+                </Link>
+                <div className="flex mx-auto">
+                  <FaPhone className="text-white my-auto mr-2 text-sm" />
+                  <p className="text-gray-500 my-auto text-sm">773 724 5263</p>
+                </div>
+              </div>
+              <div className="flex flex-col bg-neutral-900 rounded-xl p-4 flex-1 m-4">
+                <p className="text-white font-serif text-lg">
+                  Juriquilla, Qro.
+                </p>
+                <p className="text-xs mb-4 text-gray-500 text-center max-w-[80%]  mx-auto ">
+                  San Luis Potosí - Santiago de Querétaro 61, Nuevo Juriquilla,
+                  76230 Juriquilla, Qro.
+                </p>
+                <Link href="https://goo.gl/maps/uhpPwcwdaD7AxGMB9">
+                  <a target="_blank">
+                    <Button className="bg-white mx-auto mb-2 px-8 text-sm ">
+                      ¿Cómo llegar?
+                    </Button>
+                  </a>
+                </Link>
+                <div className="flex mx-auto">
+                  <FaPhone className="text-white my-auto mr-2 text-sm" />
+                  <p className="text-gray-500 my-auto text-sm">446 131 0811</p>
+                </div>
+              </div>
             </div>
-            <Link href="https://g.page/losarbolitosrestaurante?share">
-              <a target="_blank">
-                <Button className="bg-white mx-auto mt-8 px-8 ">
-                  ¿Cómo llegar?
-                </Button>
-              </a>
-            </Link>
-
             <p className="text-md mt-4 font-serif text-white">
               Horario de apertura
             </p>
